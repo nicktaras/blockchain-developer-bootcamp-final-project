24c75205c673f619134476fa78c3ac55b76f642e
Nick Taras <nicktaras@hotmail.co.uk>
2021-12-01 02:08:31 +1100

    Updates

---
+++ precache-manifest.6b070077067871e3e10d7109bf8be267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0d53a5c1ad8aadd60c6",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.9280a875.chunk.css"
+  },
+  {
+    "revision": "b0d53a5c1ad8aadd60c6",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.b0d53a5c.chunk.js"
+  },
+  {
+    "revision": "c06308d2f77916295585",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/1.b492c218.chunk.css"
+  },
+  {
+    "revision": "c06308d2f77916295585",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/1.c06308d2.chunk.js"
+  },
+  {
+    "revision": "2242dfc52fd5369ed931",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.2242dfc5.js"
+  },
+  {
+    "revision": "1746a6f23847dba1d379065f55b8e52b",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
